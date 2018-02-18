fb7c1b9254f8a018c42ee2ccb388bf9bca86a197
Jefferson Frade <jefferrson.frade@gmail.com>
2018-02-18 10:25:20 -0300

    Created the Services

---
+++ app/Services/Metrics.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class Metrics
+{
+    public function __construct()
+    {
+        // Repositories
+    }
+
+    // Metric example
+    public function count()
+    {
+        return 5;
+    }
+}
---
+++ app/Support/Charts/Chart.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Support\Charts;
+
+abstract class Chart
+{
+    protected function createDataset(string $label, array $data, string $bgColor = 'rgba(0, 115, 183, 0.7)', string $borderColor = 'rgba(0, 115, 183, 1)')
+    {
+        return [
+            "label" => $label,
+            'backgroundColor' => $bgColor,
+            'borderColor' => $borderColor,
+            "pointBorderColor" => $bgColor,
+            "pointBackgroundColor" => $bgColor,
+            "pointHoverBackgroundColor" => $bgColor,
+            "pointHoverBorderColor" => $borderColor,
+            'data' => $data,
+        ];
+    }
+
+    protected function createChart(string $name, string $type, array $size, array $labels, array  $datasets, array $options = [])
+    {
+        return app()->chartjs
+            ->name($name)
+            ->type($type)
+            ->size($size)
+            ->labels($labels)
+            ->datasets([$datasets])
+            ->options($options);
+    }
+}
